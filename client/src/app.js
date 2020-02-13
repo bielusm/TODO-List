262bcabd300afddaca0f0fd964706033f40eab4c
Mitchell Bielus <mitchellbielus@gmail.com>
2020-02-13 13:08:02 -0600

    added material-ui and updated import

--- client/src/app.js
+++ client/src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import configureStore from './configureStore';
+import configureStore from './store';
 import { Provider } from 'react-redux';
 
 const store = configureStore;
