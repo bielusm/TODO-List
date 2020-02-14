9a99042e74be9574b862cc4b65ed56bf8fe3bb9a
Mitchell Bielus <mitchellbielus@gmail.com>
2020-02-13 22:30:29 -0600

    Implemented registration to the backend added user model

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 router = express.Router();
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+User = require('../models/User');
 
 //@route POST api/users
 //@desc Register user
@@ -14,7 +17,33 @@ router.post(
     if (!errors.isEmpty())
       return res.status(400).json({ errors: errors.array() });
 
-    res.send('Success');
+    const { email, password } = req.body;
+    try {
+      //Check if user exists
+      const user = await User.findOne({ email });
+      if (user) return res.status(400).json({ error: 'User already exists' });
+
+      // Hash password
+      const SALT_ROUNDS = 10;
+      const hash = await bcrypt.hash(password.toString(), SALT_ROUNDS);
+
+      //Add User to Database
+      const newUser = new User({
+        email,
+        password
+      });
+      newUser.save();
+
+      //Construct JWT Token for ID
+      payload = { id: newUser._id };
+      const token = jwt.sign(payload, process.env.JWT_SECRET);
+
+      //Send Token to user
+      res.status(200).json({ token });
+    } catch (error) {
+      return res.status(400).json({ error: 'Server Error' });
+      console.error(error);
+    }
   }
 );
 
--- server/server.js
+++ server/server.js
@@ -4,6 +4,18 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
 
 //Init Middleware
 //Body parser
