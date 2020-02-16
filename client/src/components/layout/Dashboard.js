17ee9bd1a5a84e1efac64f9cd37c9683e88b908c
Mitchell Bielus <mitchellbielus@gmail.com>
2020-02-16 14:09:59 -0600

    Updated template

--- client/src/components/layout/Dashboard.js
+++ client/src/components/layout/Dashboard.js
@@ -1,15 +1,43 @@
 import React from 'react';
-import { Table } from 'reactstrap'
-
+import { Table, Input, Button } from 'reactstrap';
+import Moment from 'react-moment';
 
 const Dashboard = props => {
-  return <Table>
-    <tr><td>
-      Buy Groceries
-    </td></tr>
-    <tr><td>Get gas</td></tr>
-
-  </Table>;
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <th>Item</th>
+          <th>Finished</th>
+          <th>Added</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Buy Groceries</td>
+          <td>
+            <Button color="link">
+              <i class="fas fa-check fa-lg"></i>
+            </Button>
+          </td>
+          <td>
+            <Moment format="YYYY/MM/DD">09/02/2019</Moment>
+          </td>
+        </tr>
+        <tr>
+          <td>Get gas</td>
+          <td>
+            <Button color="link">
+              <i class="fas fa-check fa-lg"></i>
+            </Button>
+          </td>
+          <td>
+            <Moment format="YYYY/MM/DD">09/02/2019</Moment>
+          </td>
+        </tr>
+      </tbody>
+    </Table>
+  );
 };
 
 export default Dashboard;
