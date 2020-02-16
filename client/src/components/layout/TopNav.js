21cc1a85504fcbc3956506abf7672dc1fcd88121
Mitchell Bielus <mitchellbielus@gmail.com>
2020-02-16 14:44:21 -0600

    updated nav to seperate auth and guest links

--- client/src/components/layout/TopNav.js
+++ client/src/components/layout/TopNav.js
@@ -1,32 +1,52 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Navbar, NavbarBrand, NavItem, NavLink, Nav, Button } from 'reactstrap';
 import { Link, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-export const TopNav = props => {
-  return (
+export const TopNav = ({ authenticated }) => {
+  const authLinks = (
     <>
-      <Navbar color="light">
-        <NavbarBrand>TODO List</NavbarBrand>
-        <Nav>
-          <NavItem>
-            <NavLink tag={Link} to="/dashboard">
-              Dashboard
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink tag={Link} to="/register">
-              Register
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink tag={Link} to="/login">
-              Login
-            </NavLink>
-          </NavItem>
-        </Nav>
-      </Navbar>
+      <NavItem>
+        <NavLink tag={Link} to="/dashboard">
+          Dashboard
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink to="/logout">Logout</NavLink>
+      </NavItem>
     </>
+  );
+
+  const guestLinks = (
+    <>
+      <NavItem>
+        <NavLink tag={Link} to="/register">
+          Register
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink tag={Link} to="/login">
+          Login
+        </NavLink>
+      </NavItem>
+    </>
+  );
+
+  return (
+    <Navbar color="light">
+      <NavbarBrand>TODO List</NavbarBrand>
+      <Nav>{authenticated ? authLinks : guestLinks}</Nav>
+    </Navbar>
   );
 };
 
-export default withRouter(TopNav);
+TopNav.propTypes = {
+  authenticated: PropTypes.bool.isRequired
+};
+
+const mapStateToProps = state => ({
+  authenticated: state.user.authenticated
+});
+
+export default connect(mapStateToProps)(withRouter(TopNav));
