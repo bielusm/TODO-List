7b619d8a06310b511d6ed833cec1940edcdbbaab
Mitchell Bielus <mitchellbielus@gmail.com>
2020-02-17 14:51:57 -0600

    Fixed ObjectID error

--- server/models/Todo.js
+++ server/models/Todo.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema.Types;
 const TodoSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: String,
--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema.Types;
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true },
   hash: { type: String, required: true },
-  todos: [{ type: ObjectID, ref: 'Todo' }]
+  todos: [{ type: ObjectId, ref: 'Todo' }]
 });
 
 const User = mongoose.model('User', UserSchema);
