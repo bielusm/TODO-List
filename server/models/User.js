0d75e5b3f15217f2b4681daca897b3a05efdf7d9
Mitchell Bielus <mitchellbielus@gmail.com>
2020-02-17 14:43:44 -0600

    updated user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true },
-  hash: { type: String, required: true }
+  hash: { type: String, required: true },
+  todos: [{ type: ObjectID, ref: 'Todo' }]
 });
 
 const User = mongoose.model('User', UserSchema);
