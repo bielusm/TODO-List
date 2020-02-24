4742e6bd8b45a49d8cad5280a35fce328c7ac665
Mitchell Bielus <mitchellbielus@gmail.com>
2020-02-23 22:21:40 -0600

    added error handling to todo action

--- client/src/actions/todo.js
+++ client/src/actions/todo.js
@@ -1,13 +1,23 @@
 import { setAlert } from './alert';
 import axios from 'axios';
 
-export const addTodo = formData => async dispatch => {
+export const addTodo = formData => async (dispatch, getState) => {
   try {
     const body = JSON.stringify(formData);
-    const config = { headers: { 'content-type': 'application/json' } };
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        'x-auth-token': getState().user.token
+      }
+    };
+
     const res = await axios.post('/api/todo', body, config);
     dispatch(setAlert('Todo added', 'success'));
   } catch (error) {
-    console.error(error);
+    if (error.response) {
+      error.response.data.errors.forEach(error => {
+        dispatch(setAlert(error.msg, 'danger'));
+      });
+    } else console.error(error);
   }
 };
--- client/tests/actions/todo.test.js
+++ client/tests/actions/todo.test.js
@@ -1,10 +1,11 @@
+import user from '../../src/reducers/user';
 import { addTodo } from '../../src/actions/todo';
 import { setAlert } from '../../src/actions/alert';
-import configureStore from 'redux-mock-store';
+import configureMockStore from 'redux-mock-store';
 import ReduxThunk from 'redux-thunk';
 import moxios from 'moxios';
 import { addAlert } from '../../src/actions/alert';
-const mockStore = configureStore([ReduxThunk]);
+const mockStore = configureMockStore([ReduxThunk]);
 import { uuid } from 'uuidv4';
 
 jest.mock('uuidv4', () => ({
@@ -13,7 +14,14 @@ jest.mock('uuidv4', () => ({
 }));
 
 describe('Todo action tests', () => {
+  let store;
   beforeEach(() => {
+    store = mockStore({
+      user: {
+        token: '12345',
+        authenticated: false
+      }
+    });
     moxios.install();
   });
 
@@ -22,18 +30,41 @@ describe('Todo action tests', () => {
   });
 
   test('addTodo test', async done => {
-    const store = mockStore({});
-
     moxios.wait(() => {
       const request = moxios.requests.mostRecent();
+      expect(request.config.headers).toMatchObject({
+        'x-auth-token': '12345',
+        'Content-Type': 'application/json'
+      });
       request.respondWith({ status: 200, responseText: 'Todo added' });
     });
 
-    store.dispatch(addTodo({ name: 'name' })).then(() => {
-      const actions = store.getActions();
-      let expected = addAlert(uuid(), 'Todo added', 'success');
-      expect(actions[0]).toEqual(expected);
-      done();
+    store
+      .dispatch(addTodo({ name: 'name', description: 'description' }))
+      .then(() => {
+        const actions = store.getActions();
+        let expected = addAlert(uuid(), 'Todo added', 'success');
+        expect(actions[0]).toEqual(expected);
+        done();
+      });
+  });
+
+  test('should respond to bad request', async done => {
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 400,
+        response: { errors: [{ msg: 'Name required' }] }
+      });
     });
+
+    store
+      .dispatch(addTodo({ name: 'name', description: 'description' }))
+      .then(() => {
+        const actions = store.getActions();
+        let expected = addAlert(uuid(), 'Name required', 'danger');
+        expect(actions[0]).toEqual(expected);
+        done();
+      });
   });
 });
