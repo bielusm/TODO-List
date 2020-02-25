7c12ae65dc631c9e25eab955a334f535ad9a048c
Mitchell Bielus <mitchellbielus@gmail.com>
2020-02-25 15:39:23 -0600

    added todo reducer to store

--- client/src/store.js
+++ client/src/store.js
@@ -1,10 +1,11 @@
 import alert from './reducers/alert';
 import user from './reducers/user';
+import todo from './reducers/todo';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 
-const reducer = combineReducers({ alert, user });
+const reducer = combineReducers({ alert, user, todo });
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
