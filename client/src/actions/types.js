bacab03b684d06f077e57772ec61d4d1c54e74cf
Mitchell Bielus <mitchellbielus@gmail.com>
2020-03-28 15:27:15 -0500

    moved generic to top of file

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,3 +1,6 @@
+// GENERIC
+export const RESET_STATE = 'RESET_STATE';
+
 // ALERTS
 export const ADD_ALERT = 'ADD_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
@@ -12,6 +15,3 @@ export const REMOVE_TODO_BY_ID = 'REMOVE_TODO_BY_ID';
 export const SET_TODO = 'SET_TODO';
 export const EDIT_TODO = 'EDIT_TODO';
 export const ADD_TODO = 'ADD_TODO';
-
-// GENERIC
-export const RESET_STATE = 'RESET_STATE';
